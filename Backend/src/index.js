b9f10014e66d824e4f59a285b7adb1a8caf7a285
raisunny1314 <raisunny1314@gmail.com>
2026-08-19 11:20:24 +0530

    Update index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 
 
 app.use(cors({
-  origin: "https://portfolio-pw78lxjmf-sunnyrais-projects.vercel.app",
+  origin: "https://portfolio-sunnyrais-projects.vercel.app",
   credentials: true,
 }));
 
