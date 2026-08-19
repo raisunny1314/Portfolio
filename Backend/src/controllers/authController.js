264801633d3f5c499634c6a0c178b63bfe632529
raisunny1314 <raisunny1314@gmail.com>
2026-08-19 11:05:11 +0530

    Update authController.js

--- Backend/src/controllers/authController.js
+++ Backend/src/controllers/authController.js
@@ -56,8 +56,8 @@ const login = async (req, res) => {
         res.cookie('token', token, {
     maxAge: 5 * 60 * 60 * 1000,
     httpOnly: true,
-    secure: true,       // add karo
-    sameSite: 'none',   // add karo
+    secure: true,       
+    sameSite: 'none',   
 })
         return res.status(200).send({ success: true, token });
 
